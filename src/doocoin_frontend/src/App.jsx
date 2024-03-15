76acbefdf87b60e5c708b08da67ece29e53f837d
jakepeg <jakepeg@gmail.com>
2024-03-15 17:18:05 +0000

    frontend form

--- src/doocoin_frontend/src/App.jsx
+++ src/doocoin_frontend/src/App.jsx
@@ -1,38 +1,48 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { doocoin_backend } from 'declarations/doocoin_backend';
 
 function App() {
+  const [submitted, setSubmitted] = useState(false);
+  const SubmittedCookieName = 'eggMinted';
+
+  useEffect(() => {
+    const submittedCookie = document.cookie.split(';').find(
+      (item) => item.trim().startsWith(`${SubmittedCookieName}=`)
+    );
+
+    if (submittedCookie) {
+      setSubmitted(true);
+    }
+  }, []); 
+
   function handleSubmit(event) {
     event.preventDefault();
     const pid = event.target.elements.pid.value;
-    doocoin_backend.foundEgg(pid).then(() => {
-      // hide form
-      // show message
-      // set cookie
-    });
+    console.log("Egg clicked!"+ pid);
+    setSubmitted(true);
+    document.cookie = `${SubmittedCookieName}=true; path=/`;
+    // doocoin_backend.foundEgg(pid).then(() => {
+    //   setSubmitted(true);
+    //   set cookie
+    // });
     return false;
   }
 
   return (
     <main>
-
-<section>
-<form action="#" onSubmit={handleSubmit}>
-<div class="input-container">
-  <input id="pid" alt="Principal ID" placeholder="Enter your ICP Principal ID and click the egg..." type="text" />
- <br /><br /> <input type="image" src="egg-button.png" class="egg-button" alt="Egg Minting Button" />
- </div>
-</form>
-
-
-
-
-
-
-
-      
-</section>
-      <section id="message">Congrats, you just minted a golden egg! Check your wallet and keep an eye on your egg to see when it hatches.</section>
+      {!submitted ? (
+      <section>
+        <form action="#" onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input id="pid" alt="Principal ID" pattern="\S{63}" required placeholder="Enter your ICP Principal ID and click the egg..." type="text" />
+            <br />
+            <input type="image" src="egg-button.png" className="egg-button" alt="Egg Minting Button" />
+          </div>
+        </form> 
+      </section>
+      ) : (
+      <div id="message">Eggcelent! You minted a golden egg! Check your wallet and keep an eye on your egg to see when it hatches.</div>
+      )}
     </main>
   );
 }
